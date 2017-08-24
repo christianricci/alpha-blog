9217e19d314248a82577c6ee546aba585a98ffc2
Ricci <riccic@amazon.com>
2017-08-24 17:25:32 +1000

    create user validations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,17 +19,17 @@ class ArticlesController < ApplicationController
       flash[:success] = 'Article was successfully created'
       redirect_to article_path(@article)
     else
-        render 'new'
+      render 'new'
     end
   end
 
   def update
-        if @article.update article_params
-          flash[:success] = 'Article was successfully updated'
-          redirect_to article_path(@article)
-        else
-          render 'edit'
-        end
+    if @article.update article_params
+      flash[:success] = 'Article was successfully updated'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
   def show
@@ -43,11 +43,11 @@ class ArticlesController < ApplicationController
 
   private
 
-    def set_article
-      @article = Article.find(params[:id])
-    end
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
-    def article_params
-      params.require(:article).permit(:title, :description)
-    end
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
