47b31d472020b5a6443efe5f5a910c20827a420f
Ricci <riccic@amazon.com>
2017-09-30 22:19:17 +1000

    add user admin functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 # users_controller.rb
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
 
   def new
     @user = User.new
@@ -37,6 +38,13 @@ class UsersController < ApplicationController
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all articles created by this user have been deleted"
+    redirect_to users_path
+  end
+
   private
 
   def user_params
@@ -48,8 +56,15 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if !logged_in? && current_user != @user
+    if current_user != @user and !current_user.admin?
       flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+    end
+  end
+
+  def require_admin
+    if logged_in? and !current_user.admin?
+      flash[:danger] = "Only admin users can performn that action"
       redirect_to root_path
     end
   end
